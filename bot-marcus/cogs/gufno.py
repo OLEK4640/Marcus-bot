639dafaeae9500e7ed0364813c348ff343d7785a
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2021-04-07 12:59:51 +0100

    Improved the cog, line breaks, formatting...

--- bot-marcus/cogs/gufno.py
+++ bot-marcus/cogs/gufno.py
@@ -1,32 +1,12 @@
-import time
-poczatek = time.monotonic()
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
-import json
-import asyncio
-import aiohttp
-import random
-import os
-from math import floor
-from discord.ext.commands import CheckFailure
-from discord.ext.commands import command, has_permissions, bot_has_permissions
-from math import floor
-import urllib.parse, urllib.request, re
-from typing import Optional
-import datetime
 
-class gufno(commands.Cog):
 
-    def __init__(self, client):
-        self.client = client
-
-    #iwenty
-    @commands.Cog.listener()
-    async def on_ready(self):
+class Gufno(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
         print('Komendy administracyjne załadowane pomyślnie!.')
 
 
-
-def setup(client):
-    client.add_cog(gufno(client))
+def setup(bot):
+    bot.add_cog(Gufno(bot))
