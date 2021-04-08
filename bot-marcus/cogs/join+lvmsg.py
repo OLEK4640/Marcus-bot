c2a264464cbed234b5d9593fb05d211cfa3a8848
OLEK4640 <olekdykiel3@gmail.com>
2021-04-08 11:54:01 +0200

    próba dodania join i leave msg configow

--- bot-marcus/cogs/join+lvmsg.py
+++ bot-marcus/cogs/join+lvmsg.py
@@ -1,11 +1,29 @@
 import discord
 from discord.ext import commands
+import json
+import os
+import datetime
 
 
 class join(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         print('Komendy administracyjne załadowane pomyślnie!.')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, ctx, *, member):
+        with open("./configi.json", 'r') as configjsonFile:
+            config_data = json.load(configjsonFile)
+
+        kanal_id = int(config_data[str(ctx.guild.id)]["joinmsgchannel"])
+        kolor_hex = int(config_data[str(ctx.guild.id)]["joinmsgcolor"])
+        titel_join = int(config_data[str(ctx.guild.id)]["joinmsgtitle"])
+        deskripszon = int(config_data[str(ctx.guild.id)]["joinmsgdescription"])
+        embed = discord.Embed(title=titel_join, description=deskripszon, color=kolor_hex)
+        channel = self.bot.get_channel(kanal_id)
+        await channel.send(embed=embed)
+
+        
 
 
 def setup(bot):
