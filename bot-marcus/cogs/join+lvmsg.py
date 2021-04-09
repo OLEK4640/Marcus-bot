8ed0e4d472d2c68553e9692d41331919abefb121
OLEK4640 <olekdykiel3@gmail.com>
2021-04-09 09:16:08 +0200

    ostatnia wersja przed przerwą w rozbudowie

--- bot-marcus/cogs/join+lvmsg.py
+++ bot-marcus/cogs/join+lvmsg.py
@@ -5,26 +5,24 @@ import os
 import datetime
 
 
-class join(commands.Cog):
+class Join(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         print('Komendy administracyjne załadowane pomyślnie!.')
 
     @commands.Cog.listener()
-    async def on_member_join(self, ctx, *, member):
+    async def on_member_join(self, member):
         with open("./configi.json", 'r') as configjsonFile:
             config_data = json.load(configjsonFile)
 
-        kanal_id = int(config_data[str(ctx.guild.id)]["joinmsgchannel"])
-        kolor_hex = int(config_data[str(ctx.guild.id)]["joinmsgcolor"])
-        titel_join = int(config_data[str(ctx.guild.id)]["joinmsgtitle"])
-        deskripszon = int(config_data[str(ctx.guild.id)]["joinmsgdescription"])
+        kanal_id = int(config_data[str(member.guild.id)]["joinmsgchannel"])
+        kolor_hex = int(config_data[str(member.guild.id)]["joinmsgcolor"])
+        titel_join = int(config_data[str(member.guild.id)]["joinmsgtitle"])
+        deskripszon = int(config_data[str(member.guild.id)]["joinmsgdescription"])
         embed = discord.Embed(title=titel_join, description=deskripszon, color=kolor_hex)
         channel = self.bot.get_channel(kanal_id)
         await channel.send(embed=embed)
 
-        
-
 
 def setup(bot):
-    bot.add_cog(join(bot))
+    bot.add_cog(Join(bot))
